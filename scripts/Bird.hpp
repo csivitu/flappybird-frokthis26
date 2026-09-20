c4593b8fd10254ea5a65592d7b140d5f29f38f97
rayhan <hajeerayhan@gmail.com>
2026-09-20 23:07:27 +0530

    invert flap velocity to fly upward

--- scripts/Bird.hpp
+++ scripts/Bird.hpp
@@ -37,7 +37,7 @@ public:
     void flap() {
         if (vel <= gravity / 10.0f) {
             accel = 0.0f;
-            vel = -gravity / 4.0f;
+            vel = gravity / 4.0f;
         }
     }
 
